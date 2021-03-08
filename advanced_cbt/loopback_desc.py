69dfbcbd39e9a2b1bde1da3b2d5c31f3ce53874b
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-03-08 22:05:30 +0000

    added script to configure devices using netmiko_nornir

--- advanced_cbt/loopback_desc.py
+++ advanced_cbt/loopback_desc.py
@@ -4,8 +4,11 @@ from nornir_utils.plugins.functions import print_result # Function used to print
 
 # Initialise nornir by creating object using InitNornir function and pass in the config file
 nornir = InitNornir('config.yml')
-# Call run method on object and tell it to use netmiko and specify the command we want to send
-result = nornir.run(netmiko_send_command, command_string="show ip int br")
-# Use function to print output
+# List of commands we want to send to the devices
+config_lst = ["interface loopback100", "description configured using Nornir-Netmiko"]
+
+# Call run method on object and tell it to use netmiko and pass in the list of commands above to send to devices
+result = nornir.run(netmiko_send_config, config_commands=config_lst)
+
 print_result(result)
 
