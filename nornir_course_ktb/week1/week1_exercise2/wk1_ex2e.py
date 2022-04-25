48bf603485c5f82699795ee22f3d0ec75a22ed07
Suhaib Saeed <saeed.suhaib@googlemail.com>
2022-04-25 11:45:26 -0700

    Completed exercise 3 and added descs to ex2 and ex3

--- nornir_course_ktb/week1/week1_exercise2/wk1_ex2e.py
+++ nornir_course_ktb/week1/week1_exercise2/wk1_ex2e.py
@@ -1,5 +1,13 @@
 from nornir import InitNornir
 
+"""
+2e. Create a Python script that uses InitNornir to create a Nornir object using the newly created hosts.yaml and groups.yaml files.
+Using a for-loop loop over the Nornir hosts in inventory and print out the following attributes for each host:
+ hostname, groups, platform, username, password, and port.
+Do this directly from the Nornir host objects
+(i.e. rely on Nornir's ability to search from the host object then to group object).
+"""
+
 # Create norn object by initialising Nornir - automatically processess simpinventory files
 norn = InitNornir()
 # Get hosts object
---
+++ nornir_course_ktb/week1/week1_exercise3/wk1_ex3b.py
@@ -0,0 +1,23 @@
+from nornir import InitNornir
+
+"""
+b. Re-execute your exercise2e Python script against this new inventory.
+In other words, verify that your Python for-loop on the Nornir Host Objects properly searches from host, to group, to defaults
+ and prints out the attributes specified in exercise 2e.
+
+3
+"""
+
+# Create norn object by initialising Nornir - automatically processess simpinventory files
+norn = InitNornir()
+# Get hosts object
+hosts = norn.inventory.hosts
+
+# Loop through dictionary of hostobjects and get attributes - 
+for i in hosts:
+    print(hosts[i].hostname)
+    print(hosts[i].groups)
+    print(hosts[i].platform)
+    print(hosts[i].username)
+    print(hosts[i].password)
+    print('-' * 10)
