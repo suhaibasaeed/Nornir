514ce04d50a7fa3d8812eb281f5759a00396a0f5
Suhaib Saeed <saeed.suhaib@googlemail.com>
2022-05-09 15:12:22 -0700

    Completeted exercise 1a

--- nornir_course_ktb/week4/week4_exercise1/wk4_ex1a.py
+++ nornir_course_ktb/week4/week4_exercise1/wk4_ex1a.py
@@ -9,3 +9,40 @@ Juniper SRX                 show system uptime | match System
 
 Print out the Nornir device name and the uptime string for each of the hosts.
 """
+
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_netmiko import netmiko_send_command
+from nornir.core.filter import F
+from nornir.core.task import Result
+
+
+# Custom task which gets uptime of device
+def device_uptime(task):
+    
+    # Dict which stores platform and commands - means we don't need if statement for each
+    command = {"ios": "show version | inc uptime",
+               "nxos": "show version | inc uptime",
+               "eos": "show version | inc Uptime",
+               "junos": "show system uptime | match System"
+    }
+    # Get group name and pass into dict get method which returns command
+    cmd = command.get(task.host.groups[0].name)
+    # Execure task serially against devices
+    result = task.run(task=netmiko_send_command, command_string=cmd)
+    
+    # Print host and result - Parse through list like MultiResult object
+    print(task.host.name)
+    print(result[0])
+    
+
+if __name__ == "__main__":
+    
+    # Initialise Nornir
+    nr = InitNornir("config.yaml")
+    # Execute custom task against hosts in inventory
+    results = nr.run(task=device_uptime, name="Get device uptime")
+
+
+
+
