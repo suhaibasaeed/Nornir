b20bad97f1b7aa6830be3e2223c50673ad5e3c85
Suhaib Saeed <63662275+suhaibasaeed@users.noreply.github.com>
2022-09-23 17:41:19 +0100

    Create wk2_ex2cKTB.py

---
+++ week2/week2_exercise2/wk2_ex2cKTB.py
@@ -0,0 +1,25 @@
+from rich import print
+from nornir import InitNornir
+from nornir.core.filter import F
+from nornir_netmiko import netmiko_send_command
+
+
+def main():
+    nr = InitNornir(config_file="config.yaml")
+    filt = F(groups__contains="ios")
+    nr = nr.filter(filt)
+    my_results = nr.run(
+        task=netmiko_send_command, command_string="show run | i hostname"
+    )
+    host_results = my_results["cisco3"]
+
+    print()
+    print(type(host_results))
+    print(repr(host_results[0]))
+    # Check if it's an iterable via attribute
+    print(host_results.__iter__)
+    print()
+
+
+if __name__ == "__main__":
+    main()
