2c6659c3036942cc7296c37a02fdd6ce86d18058
Suhaib Saeed <63662275+suhaibasaeed@users.noreply.github.com>
2022-09-25 19:19:10 +0100

    Create wk6_ex5bKTB.py

---
+++ week6/week6_exercise5/wk6_ex5bKTB.py
@@ -0,0 +1,41 @@
+import os
+import random
+from netmiko import NetMikoAuthenticationException
+from nornir import InitNornir
+from nornir.core.exceptions import NornirSubTaskError
+from nornir_utils.plugins.functions import print_result
+from nornir_netmiko import netmiko_send_command
+
+
+BAD_PASSWORD = "bogus"
+
+
+def send_command(task):
+    command_mapper = {"junos": "show system uptime"}
+    cmd = command_mapper.get(task.host.platform, "show clock")
+    try:
+        task.run(task=netmiko_send_command, command_string=cmd)
+    except NornirSubTaskError as e:
+        if isinstance(e.result.exception, NetMikoAuthenticationException):
+            # Remove the failed task (so ultimately the Nornir print output is cleaner)
+            task.results.pop()
+
+            # For failed devices reset the password to the correct value using environment var
+            task.host.password = os.environ["NORNIR_PASSWORD"]
+            # Try again (with the correct password)
+            task.run(task=netmiko_send_command, command_string=cmd)
+        else:
+            return f"Unhandled exception: {e}"
+
+
+def main():
+    nr = InitNornir(config_file="config.yaml")
+    for hostname, host_obj in nr.inventory.hosts.items():
+        if random.choice([True, False]):
+            host_obj.password = BAD_PASSWORD
+    agg_result = nr.run(task=send_command)
+    print_result(agg_result)
+
+
+if __name__ == "__main__":
+    main()
